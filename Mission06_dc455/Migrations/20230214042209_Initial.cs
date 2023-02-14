62ce4f54fe75e61d5073979011bc001c9235392e
Derek Charlton <dc455@byu.edu>
2023-02-13 21:24:00 -0700

    Add project files.

---
+++ Mission06_dc455/Migrations/20230214042209_Initial.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Mission06_dc455.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "responses",
+                columns: table => new
+                {
+                    EntryID = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Category = table.Column<string>(nullable: false),
+                    Title = table.Column<string>(nullable: false),
+                    Year = table.Column<int>(nullable: false),
+                    Director = table.Column<string>(nullable: false),
+                    Rating = table.Column<string>(nullable: false),
+                    Edited = table.Column<bool>(nullable: false),
+                    LentTo = table.Column<string>(nullable: true),
+                    Notes = table.Column<string>(maxLength: 25, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_responses", x => x.EntryID);
+                });
+
+            migrationBuilder.InsertData(
+                table: "responses",
+                columns: new[] { "EntryID", "Category", "Director", "Edited", "LentTo", "Notes", "Rating", "Title", "Year" },
+                values: new object[] { 1, "Action/Adventure", "Peter Jackson", false, null, "Best battle ever!", "PG-13", "The Lord of the Rings: The Two Towers", 2004 });
+
+            migrationBuilder.InsertData(
+                table: "responses",
+                columns: new[] { "EntryID", "Category", "Director", "Edited", "LentTo", "Notes", "Rating", "Title", "Year" },
+                values: new object[] { 2, "Action/Adventure", "Joe/Anthony Russo", false, null, "Dope movie", "PG-13", "The Gray Man", 2022 });
+
+            migrationBuilder.InsertData(
+                table: "responses",
+                columns: new[] { "EntryID", "Category", "Director", "Edited", "LentTo", "Notes", "Rating", "Title", "Year" },
+                values: new object[] { 3, "Action/Adventure", "Martin Campbell", false, null, "GOAT", "PG-13", "Casino Royale", 2006 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "responses");
+        }
+    }
+}
---
+++ Mission06_dc455/Models/MovieApplicationContext.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission06_dc455.Models
+{
+    public class MovieApplicationContext : DbContext
+    {
+        public MovieApplicationContext(DbContextOptions<MovieApplicationContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<ApplicationResponse> responses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            mb.Entity<ApplicationResponse>().HasData(
+                new ApplicationResponse
+                {
+                    EntryID = 1,
+                    Category = "Action/Adventure",
+                    Title = "The Lord of the Rings: The Two Towers",
+                    Year = 2004,
+                    Director = "Peter Jackson",
+                    Rating = "PG-13",
+                    LentTo = null,
+                    Notes = "Best battle ever!"
+
+                },
+                new ApplicationResponse
+                {
+                    EntryID = 2,
+                    Category = "Action/Adventure",
+                    Title = "The Gray Man",
+                    Year = 2022,
+                    Director = "Joe/Anthony Russo",
+                    Rating = "PG-13",
+                    LentTo = null,
+                    Notes = "Dope movie"
+
+                },
+                new ApplicationResponse
+                {
+                    EntryID = 3,
+                    Category = "Action/Adventure",
+                    Title = "Casino Royale",
+                    Year = 2006,
+                    Director = "Martin Campbell",
+                    Rating = "PG-13",
+                    LentTo = null,
+                    Notes = "GOAT"
+
+                }
+                );;
+        }
+    }
+}
